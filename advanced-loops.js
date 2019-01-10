52275a3b152bbe1f151be7ba49b107fb69ad8b0b
kameic <kamei.chg@gmail.com>
2019-01-10 12:07:27 +0000

    adding loops js

---
+++ advanced-loops.js
@@ -0,0 +1,32 @@
+const basket = ['apples', 'oranges', 'grapes'];
+
+
+
+for (let i = 0; i < basket.length; i++) {
+	console.log(basket[i]);
+} 
+
+basket.forEach(item => {
+	console.log(item);
+})
+
+//for of - item is a var created in for brackets,
+//for of iterates arrays and strings (these are iterable)
+//like a for each
+for(item of basket) {
+	console.log(item)
+}
+
+
+//for in - used for object properties,
+// enurmerating lists out properties in objs (returns apples/oranges/grapes)
+// like key, values and entries
+const detailedBasket = {
+	apples: 5,
+	oranges: 10,
+	grapes: 1000,
+}
+
+for (item in detailedBasket) {
+	console.log(item)
+}
